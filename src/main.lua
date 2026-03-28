c1cfcab9bb0c7c66e838f8b4cfd280fe2ea2302b
maybe-adamant <maybe.adamant@gmail.com>
2026-03-28 16:05:09 -0400

    updating submodules

--- src/main.lua
+++ src/main.lua
@@ -27,7 +27,7 @@ public.definition = {
     tooltip  = "Always displays the current location in the UI.",
     default  = true,
     dataMutation = false,
-    modpack = "h2-modpack",
+    modpack = "speedrun",
 }
 
 -- =============================================================================
